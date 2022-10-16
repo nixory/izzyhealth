3cc9a3e8cf4567654b54b537f9b47e1f8a5c4741
Nikita Stepanovs(nixory) <nikitastepanovs777@gmail.com>
2022-10-16 22:29:00 +0300

    section moreinfo - 26min

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 // Custom scripts
+var acc = document.getElementsByClassName("moreinfo__item");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
